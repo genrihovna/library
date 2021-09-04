caacfb40138a0c0d90dbff25da262d458f1729d5
svetlana.a.titova <svetlana.a.titova@accenture.com>
2021-09-04 16:27:19 +0300

    security configuration set up

--- src/main/java/acc/roadmap1/library/config/SecurityConfig.java
+++ src/main/java/acc/roadmap1/library/config/SecurityConfig.java
@@ -1,11 +1,16 @@
 package acc.roadmap1.library.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
 import javax.sql.DataSource;
@@ -23,11 +28,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
+//        auth.inMemoryAuthentication()
+//                .withUser("user1").password(passwordEncoder().encode("user1Pass"))
+//                .authorities("ADMIN");
         auth.jdbcAuthentication().dataSource(securityDataSource)
                 .usersByUsernameQuery(
                         "SELECT username, password, enabled FROM users WHERE username=?")
                 .authoritiesByUsernameQuery(
-                        "SELECT username, role FROM authorities WHERE username=?");
+                        "SELECT username, authority FROM authorities WHERE username=?");
     }
 
     @Override
@@ -35,8 +43,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                     .antMatchers("/").permitAll()
-                    .antMatchers( "/reader").hasRole("READER")
-                    .antMatchers("/librarian/**").hasRole("ADMIN")
+                    .antMatchers( "/reader").hasAuthority("READER")
+                    .antMatchers("/librarian/**").hasAuthority("ADMIN")
                 .and()
                 .formLogin()
                     .loginPage("/login")
@@ -50,4 +58,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().accessDeniedHandler(accessDeniedHandler)
                 ;
     }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+
+    @SuppressWarnings("deprecation")
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//    }
 }
