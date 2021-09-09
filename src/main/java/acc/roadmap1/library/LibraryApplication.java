63dc12b849db943f2bcf2b9acd7eefed8eb88c17
svetlana.a.titova <svetlana.a.titova@accenture.com>
2021-09-09 18:26:49 +0300

    commonController test

--- src/main/java/acc/roadmap1/library/LibraryApplication.java
+++ src/main/java/acc/roadmap1/library/LibraryApplication.java
@@ -1,13 +1,18 @@
 package acc.roadmap1.library;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @ComponentScan(basePackages = "acc.roadmap1.library")
 @SpringBootApplication
 public class LibraryApplication {
+
+//	@Autowired
+//	JdbcTemplate jdbcTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
--- src/main/java/acc/roadmap1/library/config/SecurityConfig.java
+++ src/main/java/acc/roadmap1/library/config/SecurityConfig.java
@@ -3,20 +3,19 @@ package acc.roadmap1.library.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
 import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
+//@EnableJpaRepositories(basePackages="acc.roadmap1.library", entityManagerFactoryRef="emf")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
