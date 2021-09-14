0e39ec96c13f888f3b99dd020dab64a74ef16101
svetlana.a.titova <svetlana.a.titova@accenture.com>
2021-09-14 21:08:58 +0300

    commonController test

--- src/test/java/acc/roadmap1/library/LibraryApplicationTests.java
+++ src/test/java/acc/roadmap1/library/LibraryApplicationTests.java
@@ -1,13 +1,36 @@
 package acc.roadmap1.library;
 
+import acc.roadmap1.library.controller.CommonController;
+import acc.roadmap1.library.service.BookService;
+import acc.roadmap1.library.service.ReaderService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.sql.DataSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class LibraryApplicationTests {
 
+	@Autowired
+	private CommonController commonController;
+
+	@MockBean
+	private DataSource securityDataSource;
+
+	@MockBean
+	private BookService bookService;
+
+	@MockBean
+	private ReaderService readerService;
+
 	@Test
 	void contextLoads() {
+//		assertThat(commonController).isNotNull();
 	}
 
 }
--- src/test/java/acc/roadmap1/library/controller/CommonControllerTest.java
+++ src/test/java/acc/roadmap1/library/controller/CommonControllerTest.java
@@ -1,37 +1,58 @@
 package acc.roadmap1.library.controller;
 
-import acc.roadmap1.library.dao.BookRepo;
-import acc.roadmap1.library.service.BookServiceImpl;
+import acc.roadmap1.library.model.Book;
+import acc.roadmap1.library.service.BookService;
+import acc.roadmap1.library.service.ReaderService;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import javax.sql.DataSource;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(CommonController.class)
-//@DataJpaTest
-//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class CommonControllerTest {
 
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @Autowired
-//    private DataSource securityDataSource;
-//
-//    @InjectMocks
-//    private BookServiceImpl bookService;
-//
-//    @Mock
-//    private BookRepo repository;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private DataSource securityDataSource;
+
+    @MockBean
+    private BookService bookService;
+
+    @MockBean
+    private ReaderService readerService;
 
     @Test
-    void welcomePage() {
+    void welcomePage() throws Exception {
+        List<Book> books = Arrays.asList(new Book(100, "title", "author", (short)1990, null, false),
+                new Book(101, "title1", "author", (short)2000, null, false),
+                new Book(102, "title2", "author", (short)2010, null, false));
+        when(bookService.findAll()).thenReturn(books);
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/")
+                .accept(MediaType.APPLICATION_JSON);
+        MvcResult result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+//                .andExpect(content()
+//                        .json("[{id: 100,title:title,author:author,published:1990}," +
+//                                "{id: 101,title:title,author:author,published:2000}," +
+//                                "{id: 102,title:title,author:author,published:2010}]", false))
+//                .andExpect(content().jsonPath("/html/body/div[3]/table/tbody/tr[1]/td[2]"))
+                .andReturn();
 
     }
 
