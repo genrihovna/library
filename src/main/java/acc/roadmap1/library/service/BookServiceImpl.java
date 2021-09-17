7a55a734cecc40cc7fc9a592e0c9ed0bb8b8e3a8
Rinat Muhamedgaliev <rinatmuhamedgaliev@gmail.com>
2021-09-18 01:25:27 +0300

    Fix books controller

--- src/main/java/acc/roadmap1/library/service/BookServiceImpl.java
+++ src/main/java/acc/roadmap1/library/service/BookServiceImpl.java
@@ -1,5 +1,6 @@
 package acc.roadmap1.library.service;
 
+import acc.roadmap1.library.controller.dto.BookDTO;
 import acc.roadmap1.library.repository.BookRepository;
 import acc.roadmap1.library.model.Book;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,11 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
+    public Book create(BookDTO book) {
+        return bookRepository.save(new Book(book.getAuthor(), book.getPublished(), book.getTitle()));
+    }
+
+    @Override
     public Book findById(long theId) {
         Optional<Book> result = bookRepository.findById(theId);
         Book theBook = null;
@@ -32,11 +38,6 @@ public class BookServiceImpl implements BookService {
             throw new RuntimeException("Did not get book - " + theId);
         }
         return theBook;
-    }
-
-    @Override
-    public Book save(Book theBook) {
-        return bookRepository.save(theBook);
     }
 
     @Override
