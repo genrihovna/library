1134c00f3422f6d07b471fcf722d8ab3bf1f47f3
svetlana.a.titova <svetlana.a.titova@accenture.com>
2021-09-19 16:27:27 +0300

    root controller - testing by xpath

--- src/test/java/acc/roadmap1/library/RootControllerTest.java
+++ src/test/java/acc/roadmap1/library/RootControllerTest.java
@@ -9,9 +9,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -21,14 +19,67 @@ public class RootControllerTest extends BaseMockTest {
     private MockMvc mockMvc;
 
     @Test
-    void welcomePage() throws Exception {
+    void welcomePageTest() throws Exception {
         RequestBuilder request = MockMvcRequestBuilders
                 .get("/")
-                .accept(MediaType.APPLICATION_JSON);
+                .accept(MediaType.APPLICATION_JSON)
+                ;
         mockMvc.perform(request)
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
-                .andExpect(xpath("//div[contains(@class, 'is user')]").exists())
+                .andExpect(content().contentType("text/html;charset=UTF-8"))
+                .andExpect(xpath("//div[contains(@class, 'is reader')]").exists())
+                .andExpect(xpath("//div[3]/h1").string("Welcome to Library Page"))
                 .andReturn();
+    }
+
+    @Test
+    void getLoginPageTest() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/login")
+                .accept(MediaType.APPLICATION_JSON)
+                ;
+        mockMvc.perform(request)
+                .andExpect(xpath("//div[3]/h3").string("Library Login Page"));
+    }
+
+    @Test
+    void getRegisterPageTest() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/register")
+                .accept(MediaType.APPLICATION_JSON)
+                ;
+        mockMvc.perform(request)
+                .andExpect(xpath("//div[2]/h3").string("Library Login Page"));
+    }
+
+    @Test
+    void getAdminRegisterPageTest() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/register/admin")
+                .accept(MediaType.APPLICATION_JSON)
+                ;
+        mockMvc.perform(request)
+                .andExpect(xpath("//div[2]/h3").string("Library Login Page"));
+    }
+
+    @Test
+    void registerNewUserTest() throws Exception {
+        RequestBuilder registerUser = MockMvcRequestBuilders
+                .post("/register")
+//                .param(username, "Username")
+//                .param(name, "Name")
+//                .param(password, "Password")
+                ;
+        mockMvc.perform(registerUser)
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+    }
+
+    @Test
+    void registerAdminTest() {
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/register/admin")
+                .accept(MediaType.APPLICATION_JSON)
+                ;
     }
 }
