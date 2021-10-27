59dfb30166543bd158dca45d59a148d13a58d8a7
Rinat Muhamedgaliev <rinatmuhamedgaliev@gmail.com>
2021-10-27 22:45:54 +0300

    Upd take in and takout

--- src/main/java/acc/roadmap1/library/controller/ErrorMapping.java
+++ src/main/java/acc/roadmap1/library/controller/ErrorMapping.java
@@ -1,26 +1,44 @@
 package acc.roadmap1.library.controller;
 
+import acc.roadmap1.library.LibraryException;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class ErrorMapping {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ErrorMapping.class);
 
     @ExceptionHandler(Throwable.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String exception(Throwable throwable, Model model) {
+
+        LOGGER.error(ExceptionUtils.getStackTrace(throwable));
+
         String errorMessage =
                 (throwable != null && throwable.getMessage() != null && !throwable.getMessage().isBlank() ?
                         throwable.getMessage() :
                         "Unknown error");
         model.addAttribute("errorMessage", errorMessage);
         model.addAttribute("errorCode", 500);
-
-        throwable.printStackTrace();
-
         return "error";
+    }
+
+    @ExceptionHandler(LibraryException.class)
+    public ModelAndView handleProjectException(LibraryException e, Model model) {
+
+        ModelMap modelMap = new ModelMap();
+
+        modelMap.addAttribute("error", e.getMessage());
+        modelMap.addAttribute("errorCode", e.getErrorCode());
+
+        return new ModelAndView("redirect:" + e.getRedirectPage());
     }
 }
--- src/main/java/acc/roadmap1/library/controller/RootController.java
+++ src/main/java/acc/roadmap1/library/controller/RootController.java
@@ -5,13 +5,16 @@ import acc.roadmap1.library.model.ApplicationUserDetails;
 import acc.roadmap1.library.model.Book;
 import acc.roadmap1.library.service.BookService;
 import acc.roadmap1.library.service.SecurityService;
+import org.dom4j.rule.Mode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.security.RolesAllowed;
 import java.util.List;
@@ -34,6 +37,7 @@ public class RootController {
                               @AuthenticationPrincipal ApplicationUserDetails userDetails) {
         List<Book> books = bookService.findBooksWithStatusForCurrentUser(userDetails);
         model.addAttribute("books", books);
+
         return "index";
     }
 
@@ -45,6 +49,7 @@ public class RootController {
     @GetMapping("/register")
     public String getRegisterPage(Model model) {
         model.addAttribute("user", new RegisterAccount());
+        model.addAttribute("error", "1");
         return "register";
     }
 
@@ -55,10 +60,10 @@ public class RootController {
     }
 
     @PostMapping("/register")
-    public String registerNewUser(@ModelAttribute("user") RegisterAccount registerAccount) {
+    public ModelAndView registerNewUser(@ModelAttribute("user") RegisterAccount registerAccount, ModelMap model) {
         securityService.createReaderAccount(registerAccount.getUsername(), registerAccount.getPassword(),
                 registerAccount.getName());
-        return "redirect:/login";
+        return new ModelAndView("redirect:/login");
     }
 
     @RolesAllowed("ADMIN")
